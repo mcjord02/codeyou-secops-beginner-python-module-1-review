e461973c683be74586c323c394dc3e12a8a44709
mcjord02 <https://github.com/mcjord02/My-New-Repo.git>
2026-04-19 11:48:19 -0400

    Completed Python step 3

--- security_log_classifier.py
+++ security_log_classifier.py
@@ -2,3 +2,21 @@ with open("logins.txt") as f:
     lines = f.readlines()
 
 print(f"Loaded {len(lines)} login records.")
+
+successful_logins = 0
+failed_logins = 0
+
+for line in lines:
+    parts = line.strip().split()
+   
+    if len(parts) < 3: continue
+    username, ip, result = parts[0], parts[1], parts[2]
+    print(f"User: {username}, IP: {ip}, Result: {result}")
+
+    if parts[2] == "FAILURE":
+        failed_logins += 1
+    else:
+        successful_logins += 1
+
+print(f"Total successful logins: {successful_logins}")
+print(f"Total failed logins: {failed_logins}")
